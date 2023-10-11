a3b4383ef481cbd5e58b8a6fd1de419b4bd7b077
IronLightning85 <gochenaurbrennan@gmail.com>
2023-10-11 10:25:53 -0400

    Defined equations arr and added 2 functions to add/remove from equation arr

---
+++ calculator.js
@@ -0,0 +1,13 @@
+//array for equation. ex. [3, "+", 6]
+let equation = [];
+
+//functions to add/remove element in array
+function equationAdd(ele)
+{
+    equation.push(ele);
+}
+
+function equationremove()
+{
+    equation.pop();
+}
