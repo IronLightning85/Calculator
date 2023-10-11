57c7461e610ec58d14d254dbc5d3a5d2f0d9a541
IronLightning85 <gochenaurbrennan@gmail.com>
2023-10-11 10:45:10 -0400

    Added clear and combine numbers funtions

--- calculator.js
+++ calculator.js
@@ -11,3 +11,23 @@ function equationremove()
 {
     equation.pop();
 }
+
+function equationClear()
+{
+    equation = [];
+}
+
+
+//combines numbers
+function combineNumbers()
+{
+    for(let i = 1; i < equation.length; i++)//iterates length of equation arr
+    {
+        if(equation[i-1].isFinite() && equation[i].isFinite())//checks if equation[i] and the index beofore it are numbers
+        {
+            equation[i-1] = '' + equation[i-1] + equation[i];//combines equation[i] and the index beofore
+            equation.splice[i, 1];//removes element at index i
+            i--;//subtracts one from i so it doesnt skip a number
+        }
+    }
+}
