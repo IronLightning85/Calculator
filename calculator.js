846625aa0ab5b64bfadc28968bee2caa4d38028c
IronLightning85 <gochenaurbrennan@gmail.com>
2023-10-12 15:19:39 -0400

    removed console.log statements, implemented backend part of histroy function

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
 //array for equation. ex. [3, "+", 6]
 let equation = [];
+let historyArr = [];
 
 //functions to add/remove element in array
 function equationAdd(ele)
@@ -33,14 +34,23 @@ for(let i = 0; i < equation.length; i++)
 document.getElementById("output").innerHTML = output;
 }
 
+function updatehistory()//called after user hits = and code merges numbers
+{
+    let equationString = ""
+    for(let i = 0; i < equation.length; i++)//iterate through equation and add to string
+    {
+        equationString+=equation[i];
+    }
+    historyArr.push(equationString);//add the string to the hsitroy arr. history arr now array of strings
+
+    //implement a display for history
+}
+
 //combines numbers
 function startEquation()//merge numbers and turn them back into numbers, not strings
 {
     for(let i = 1; i < equation.length + 1;i++)//iterates length of equation arr minus one so last element is checked
-    {
-        console.log(equation);
-        console.log(i)
-        
+    {    
         if(isFinite(equation[i-1]) && isFinite(equation[i]) || equation[i-1] == "." && isFinite(equation[i]) || isFinite(equation[i-1]) && equation[i] == "." || equation[i-1] == "_" && isFinite(equation[i]))//checks if equation[i] and the index before it are numbers or periods or _ which will turn into negatives
         {
             if(equation[i-1] == '_')
@@ -53,9 +63,7 @@ function startEquation()//merge numbers and turn them back into numbers, not str
             equation.splice(i, 1);//removes element at index i
             i--;//subtracts one from i so it doesnt skip a number
         }
-
     }
-    console.log(equation);
 
 
     //iterates through array to turn numbers into floats using parsefloat
@@ -66,7 +74,7 @@ function startEquation()//merge numbers and turn them back into numbers, not str
             equation[j] = parseFloat(equation[j]);//turn item at index j into float if it is finite
         }
     }
-    console.log(equation);
+    updatehistory();
 
     
     updateText()
@@ -82,7 +90,6 @@ function startEquation()//merge numbers and turn them back into numbers, not str
             document.getElementById("output").innerHTML = "Invalid";
         }
     }
-    console.log(equation);
 mathy();
 }
 
